79d7455a69a0fbf1851691b0f6a7a0aa41913d66
ArnoldsK <arnolds0klavenieks@gmail.com>
2026-02-27 02:50:15 +0200

    Add loadExtensions option to knex migrations configuration for improved compatibility with JavaScript files

--- app/src/db/knex.ts
+++ app/src/db/knex.ts
@@ -21,5 +21,6 @@ export const createDb = (env: Env): Db =>
     migrations: {
       tableName: "knex_migrations",
       directory: `${__dirname}/migrations`,
+      loadExtensions: [".js"],
     },
   });
