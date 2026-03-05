b17fc4643e7350c74f8015be948b4fcb0c2c839b
ArnoldsK <arnolds0klavenieks@gmail.com>
2026-03-06 00:37:15 +0200

    Enhance coinflip command: update to version 2, add user guess option for coin flip results, and improve response messages based on user input.

--- app/src/discord/commands/definitions/coinflip.ts
+++ app/src/discord/commands/definitions/coinflip.ts
@@ -2,18 +2,43 @@ import { SlashCommandBuilder } from "discord.js";
 
 import { defineCommand } from "@app/discord/commands/defineCommand";
 
+enum OptionName {
+  Guess = "guess",
+}
+
 export default defineCommand({
-  version: 1,
+  version: 2,
 
   data: new SlashCommandBuilder()
     .setName("coinflip")
-    .setDescription("Flip a coin"),
+    .setDescription("Flip a coin")
+    .addStringOption((opt) =>
+      opt
+        .setName(OptionName.Guess)
+        .setDescription("Guess the result of the coin flip")
+        .setChoices(
+          { name: "Heads", value: "Heads" },
+          { name: "Tails", value: "Tails" },
+        ),
+    ),
 
   execute: async (_ctx, interaction) => {
+    const guess = interaction.options.getString(OptionName.Guess);
     const result = Math.random() < 0.5 ? "Heads" : "Tails";
 
+    if (!guess) {
+      await interaction.reply({
+        content: `${result}`,
+      });
+
+      return;
+    }
+
     await interaction.reply({
-      content: `${result}`,
+      content:
+        guess === result
+          ? `Guessed ${guess}!`
+          : `Guessed ${guess} but got ${result}`,
     });
   },
 });
